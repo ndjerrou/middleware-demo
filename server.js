dc1859924d9d5229afa08b78c3b3f688701b2600
ndjerrou <n.djerrou@gmail.com>
2021-09-17 11:19:37 +0100

    POST route added - show req.body

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const app = express();
 
+const users = [
+  { name: 'Nissim', age: 30, likes: ['football', 'hockey'] },
+  { name: 'Chloe', age: 40, likes: ['Box', 'Cars'] },
+  { name: 'Rachid', age: 50, likes: ['Painting', 'Hollidays'] },
+];
+
 app.get('/', (req, res) => {
   res.send('Welcome');
+});
+
+app.post('/users', (req, res) => {
+  console.log(req.body);
+  res.send(users);
 });
 
 const port = process.env.port || 3000;
